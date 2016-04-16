ce4f7c9b5c52ef3d963a4e9b6a60b76ab0f4bce3
goldpbear <tcroxson@gmail.com>
2016-04-16 16:45:27 -0700

    add esri map

--- reports/public/js/controllers/MainCtrl.js
+++ reports/public/js/controllers/MainCtrl.js
@@ -3,5 +3,15 @@
 angular.module('angelhack')
 	   .controller('MainController', function($scope) {
 
-    $scope.tagline = "It's working"; 
+		   	var map;
+			require(["esri/map"], function(Map) {
+			  map = new Map("mapDiv", {
+			    center: [-56.049, 38.485],
+			    zoom: 3,
+			    basemap: "streets"
+			  });
+			});
+
+	   		$scope.tagline = "It's working"; 
+
 });
