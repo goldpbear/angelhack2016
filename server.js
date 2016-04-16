4b44b2b7d2b94473e6985959898c862c014cf58a
Steven Peng <stpeng@users.noreply.github.com>
2016-04-16 15:13:49 -0700

    init data

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+// express
+
 var express = require('express');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+
+// data
+
+var Data = require('./data');
+
+var dataOptions = {
+  url:        process.env.DATA_URL || 'localhost',
+  port:       process.env.DATA_PORT || 5432
+};
+
+var data = new Data(dataOptions);
+
+
+// routes
 
 var reportsRouter = express.Router();
 var overviewRouter = express.Router();
-
-app.listen(PORT);
 
 app.use('/reports', reportsRouter);
 app.use('/overview', overviewRouter);
