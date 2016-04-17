fc910309ed990af4be63744db01371489185f0a1
Steven Peng <stpeng@users.noreply.github.com>
2016-04-16 18:45:16 -0700

    cors

--- server.js
+++ server.js
@@ -34,7 +34,13 @@ var reportsRouter = express.Router();
 var overviewRouter = express.Router();
 var apiRouter = express.Router();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use('/reports', reportsRouter);
 app.use('/overview', overviewRouter);
